4510add62022604558e7ddb6e10728b3a83e3c0b
Rupert Walker <RupertCW@gmail.com>
2024-02-09 22:45:46 +0000

    fix: Adjust default Freelancer path to vanilla in the interests of being mod-agnostic

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace FLScanIE.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Discovery\\FreelancerSDK")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files (x86)\\Microsoft Games\\Freelancer")]
         public string setFlDir {
             get {
                 return ((string)(this["setFlDir"]));
